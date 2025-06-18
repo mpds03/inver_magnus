c5c56f50b46f0672e036d0958387673e0e370dac
Frikypao <samael4445@gmail.com>
2025-06-18 18:31:13 -0500

    areglo del boton hamburguesa  para la web

--- views/barraBusqueda.php
+++ views/barraBusqueda.php
@@ -192,8 +192,7 @@
     </div> <!-- fin de caja madre-->
 
     <!-- Scripts de Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- views/productinfo.php
+++ views/productinfo.php
@@ -279,8 +279,7 @@
     </div>
     <!--footer-->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/productinfo.js"></script>
 </body>
 
